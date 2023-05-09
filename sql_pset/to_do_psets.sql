df5029e469db1215e7437aa49c27263bd23d0437
Karandeep Singh <karan.redrock@gmail.com>
2023-05-09 14:41:57 +0530

    add problem list to solve
    7 new problems defined

---
+++ sql_pset/to_do_psets.sql
@@ -0,0 +1,16 @@
+/*
+ToDo
+
+1. Encryption and decryption of columns in SQL server.
+2. Pivots in SQL - PIVOT, UNPIVOT
+	3. Machine id vs Months - count of failures
+3. Handle SCD Type 2. Case study with stored proc
+4. Optimal filtering / join, select 1 from table where outer_t.id=inner_t.id.
+	Optimal vs non Optimal ways of SQL.
+5. Triggers in SQL 
+	Write, Insert , Delete , Update.
+	Search about the rest.
+6. Dealing with XML / JSON columns to parse information.
+7. Realtional vs No SQL. where does relational still win?
+
+*/
