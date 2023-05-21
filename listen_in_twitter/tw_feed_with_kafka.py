9f8d8be871c99e23b30ff26f5fe8b9cba96fd9e0
Karandeep Singh <karan.redrock@gmail.com>
2023-05-21 15:51:40 +0530

    kafka twitter consumer

---
+++ listen_in_twitter/tw_feed_with_kafka.py
@@ -0,0 +1,8 @@
+import textblob
+import tweepy
+import kafka
+import pyspark
+
+# https://github.com/youheekil/Twitter-Streaming-with-Apache-Kafka-Docker-and-Python
+
+https://youheekil.github.io/running-kafka-docker/
